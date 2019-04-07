3f67a2c35cd0f9a2c112d8f178bdca99e4033f80
anlerandy <alerandy@student.42.fr>
2019-04-07 09:42:01 +0200

    Updated: ft_strnstr.

--- libui/libft/srcs/ft_strnstr.c
+++ libui/libft/srcs/ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alerandy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 14:47:44 by alerandy          #+#    #+#             */
-/*   Updated: 2017/11/15 19:02:30 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/04/07 09:41:18 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t		i;
 
 	i = 0;
-	if (!haystack[i] || len == 0)
-		return (NULL);
-	if (!needle[i])
+	if (!*needle || len == 0)
 		return ((char*)haystack);
 	while (haystack[i] == needle[i] && haystack[i] && i < len)
 	{
-		i++;
+		++i;
 		if (i == len + 1 || !needle[i])
 			return ((char*)haystack);
 	}
+	if (!*haystack || len - 1 == 0)
+		return (NULL);
 	return (ft_strnstr(haystack + 1, needle, len - 1));
 }
