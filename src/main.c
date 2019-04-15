295b7ef63f8bc11bb3ee4b034569bcf225dda9e9
Guilhem Smith <gsmith@student.42.fr>
2019-04-15 10:27:38 +0200

    Added auteur file, gitignore commented.

--- src/main.c
+++ src/main.c
@@ -6,21 +6,21 @@ int main()
 	SDL_Event e;
 	int       flag;
 
-  win = ui_new_window();
-  while(1)
+	win = ui_new_window();
+	while(1)
 	{
 		flag = 1;
 		while (flag || SDL_PollEvent(&e))
 		{
 			if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN &&
-				e.key.keysym.sym == SDLK_ESCAPE))
+						e.key.keysym.sym == SDLK_ESCAPE))
 				exit(1);
 			if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN)
 				exit(1);
 			flag = 0;
 		}
 		ft_memset(win->s->pixels, 0xfff,
-			(sizeof(int) * win->s->w * win->s->h));
+				(sizeof(int) * win->s->w * win->s->h));
 		SDL_UpdateWindowSurface(win->w);
 	}
 }
