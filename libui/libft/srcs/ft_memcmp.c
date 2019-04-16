8250270e5b6e345e5afc38d2b8313fd5f32beea7
anlerandy <alerandy@student.42.fr>
2019-04-16 11:06:18 +0200

    Updated: ft_memcmp  WIP.

--- libui/libft/srcs/ft_memcmp.c
+++ libui/libft/srcs/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 13:14:54 by alerandy          #+#    #+#             */
-/*   Updated: 2019/04/07 11:27:33 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/04/11 15:09:33 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,50 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*ptr1;
-	unsigned char	*ptr2;
+	unsigned long int	*ptr1;
+	unsigned long int	*ptr2;
+	unsigned long int	src;
+	unsigned long int	dst;
+	size_t				len;
+	unsigned char		*tmp1;
+	unsigned char		*tmp2;
 
-	i = 0;
-	ptr1 = (unsigned char*)s1;
-	ptr2 = (unsigned char*)s2;
-	while (++i < n)
-		if (ptr1[i] != ptr2[i])
-			return (ptr1[i] - ptr2[i]);
-	return (0);
+	len = n / 8 + 1;
+	n -= (len - 1) * 8;
+	ptr1 = (unsigned long int*)s1;
+	ptr2 = (unsigned long int*)s2;
+	while (--len)
+	{
+		src = *ptr1++;
+		dst = *ptr2++;
+		if (src != dst)
+		{
+			tmp1 = (unsigned char*)(src);
+			tmp2 = (unsigned char*)(dst);
+			if (*tmp1 != *tmp2)
+				return (*tmp1 - *tmp2);
+			if (tmp1[1] != tmp2[1])
+				return (tmp1[1] - tmp2[1]);
+			if (tmp1[2] != tmp2[2])
+				return (tmp1[2] - tmp2[2]);
+			if (tmp1[3] != tmp2[3])
+				return (tmp1[3] - tmp2[3]);
+			if (tmp1[4] != tmp2[4])
+				return (tmp1[4] - tmp2[4]);
+			if (tmp1[5] != tmp2[5])
+				return (tmp1[5] - tmp2[5]);
+			if (tmp1[6] != tmp2[6])
+				return (tmp1[6] - tmp2[6]);
+			if (tmp1[7] != tmp2[7])
+				return (tmp1[7] - tmp2[7]);
+		}
+	}
+	tmp1 = (unsigned char*)ptr1;
+	tmp2 = (unsigned char*)ptr2;
+	while (--n && *tmp1 == *tmp2)
+	{
+		++tmp1;
+		++tmp2;
+	}
+	return (tmp1 - tmp2);
 }
