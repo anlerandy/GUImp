16419d2622eab9af5e5a64c123628879823b73d5
anlerandy <alerandy@student.42.fr>
2019-04-22 03:25:54 +0200

    Fixed: ft_strncmp.

--- libui/libft/srcs/ft_strncmp.c
+++ libui/libft/srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 15:04:24 by alerandy          #+#    #+#             */
-/*   Updated: 2019/04/21 17:01:49 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/04/22 03:18:13 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int				i;
+	size_t			i;
+	size_t			j;
 	unsigned char	*str1;
 	unsigned char	*str2;
-	int				len1;
-	int				len2;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
+	j = ft_strlen(s1);
+	i = ft_strlen(s2);
+	j = j < i ? j : i;
+	i = 0;
+	j = n < j ? n : j;
 	if (n == 0)
 		return (0);
-	if (n > (len2 = ft_strlen(s2)) || n > (len1 = ft_strlen(s1)))
-		n = len1 < len2 ? len1 : len2;
-	i = 0;
-	while (((unsigned long int)(str1 + i) & \
+	while (j <= i && ((unsigned long int)(str1 + i) & \
 			(sizeof(unsigned long int) - 1)) != 0 && \
 			((unsigned long int)(str2 + i) & \
-			(sizeof(unsigned long int) - 1)) != 0 && \
-			--n)
+			(sizeof(unsigned long int) - 1)) != 0 )
 	{
 		if (str1[i] != str2[i] || (!str1[i] || !str2[i]))
 			return (str1[i] - str2[i]);
 		++i;
 	}
-	return (ft_memcmp((void *)(str1 + i), (void *)(str2 + i), n));
+	return (ft_memcmp((void *)(str1 + i), (void *)(str2 + i), j - i));
 }
