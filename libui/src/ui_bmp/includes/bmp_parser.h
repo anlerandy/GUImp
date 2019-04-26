5707658ed0e8312d2dd2a26880d91c7897f72fe7
anlerandy <alerandy@student.42.fr>
2019-04-26 15:27:15 +0200

    Updated: parser.h.

--- libui/src/ui_bmp/includes/bmp_parser.h
+++ libui/src/ui_bmp/includes/bmp_parser.h
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 13:31:11 by alerandy          #+#    #+#             */
-/*   Updated: 2019/04/22 13:33:35 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/04/26 15:23:36 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 # define BMP_PARSER
 
 #include "libft.h"
+
+typedef struct	s_ui_bmp {
+	short	bfType;
+	int		bfSize;
+	short	bfReserved1;
+	short	bfReserved2;
+	int		bfOffBits;
+}				t_ui_bmp;
 
 #endif
