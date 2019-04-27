151451cc5aff9cda316843e22a6c7e9282dda3fa
anlerandy <alerandy@student.42.fr>
2019-04-27 13:19:34 +0200

    Updated: Packed structure.

--- libui/src/ui_bmp/includes/bmp_parser.h
+++ libui/src/ui_bmp/includes/bmp_parser.h
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 13:31:11 by alerandy          #+#    #+#             */
-/*   Updated: 2019/04/27 13:12:44 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/04/27 13:18:53 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 
 #include "libft.h"
 
-typedef struct	s_bmp_file {
+typedef struct __attribute__((__packed__))	s_bmp_file {
 	unsigned char	bfType1;
 	unsigned char	bfType2;
 	unsigned int	bfSize;
 	unsigned short	bfReserved1;
 	unsigned short	bfReserved2;
 	unsigned int	bfOffBits;
-}				t_bmp_file;
+}											t_bmp_file;
 
-typedef struct	s_bmp_image {
+typedef struct __attribute__((__packed__))	s_bmp_image {
 	unsigned int	biSize;
 	int				width;
 	int				height;
@@ -36,13 +36,13 @@ typedef struct	s_bmp_image {
 	int				biYPelsPerMeter;
 	unsigned int	biClrUsed;
 	unsigned int	biClrImportant;
-}				t_bmp_image;
+}											t_bmp_image;
 
 
 
-typedef struct	s_ui_bmp {
+typedef struct								s_ui_bmp {
 	t_bmp_file	header;
 	t_bmp_image	bmp;
-}				t_ui_bmp;
+}											t_ui_bmp;
 
 #endif
