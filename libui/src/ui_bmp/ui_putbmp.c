56be30afe5eb9a3f7a4a208d5ce9a93551e77b20
anlerandy <alerandy@student.42.fr>
2019-04-28 14:19:21 +0200

    Fixed: color and order.

--- libui/src/ui_bmp/ui_putbmp.c
+++ libui/src/ui_bmp/ui_putbmp.c
@@ -6,28 +6,46 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 03:16:49 by alerandy          #+#    #+#             */
-/*   Updated: 2019/04/28 04:11:58 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/04/28 13:35:14 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bmp_parser.h"
 
-void	ui_putbmp( t_bmp_header header, t_bmp_file bmp)
+void	putCompression(unsigned compression)
 {
-	ft_putstr("bfType: ");
+	ft_putstr("Compression: ");
+	if (compression == 0)
+		ft_putendl("Uncompressed");
+	if (compression == 1)
+		ft_putendl("RLE-8");
+	if (compression == 2)
+		ft_putendl("RLE-4");
+	if (compression == 3)
+		ft_putendl("Bitfields");
+}
+
+void	ui_putbmp(t_bmp_header header, t_bmp_file bmp)
+{
+	ft_putstr("Type de fichier: ");
 	ft_putchar(header.bfType[0]);
 	ft_putchar(header.bfType[1]);
-	ft_putstr("\nbfSize: ");
-	ft_putnbr(header.bfSize);
-	ft_putstr("\nbfOffBits: ");
+	ft_putstr("\nTaille du fichier: ");
+	ft_putnbr(header.bfSize / 1000000);
+	ft_putstr(" Mb\nbfOffBits: ");
 	ft_putnbr(header.bfOffBits);
 	ft_putstr("\nwidth: ");
 	ft_putnbr(bmp.width);
-	ft_putstr("\nheight: ");
+	ft_putstr("px\nheight: ");
 	ft_putnbr(bmp.height);
-	ft_putstr("\n");
+	ft_putstr("px\n");
 	ft_putnbr(bmp.biBitCount);
-	ft_putendl("bits.");
+	ft_putendl("bits");
 	ft_putnbr(bmp.width * bmp.height);
-	ft_putendl(" Pixels.");
+	ft_putendl(" Pixels");
+	putCompression(bmp.biCompression);
+	ft_putstr("Used color: ");
+	ft_putnbr(bmp.biClrUsed);
+	ft_putstr("\nBMP Version: ");
+	ft_putnbr(header.bfOffBits / 1000000);
 }
