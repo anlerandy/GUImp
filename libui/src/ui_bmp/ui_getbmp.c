54f73fcc14374e4f7e47cb5fd13958c2620ef259
anlerandy <alerandy@student.42.fr>
2019-05-02 20:27:55 +0200

    Updated: Separate the readers. Added: 1bit reader.

--- libui/src/ui_bmp/ui_getbmp.c
+++ libui/src/ui_bmp/ui_getbmp.c
@@ -6,36 +6,43 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 03:54:28 by alerandy          #+#    #+#             */
-/*   Updated: 2019/04/28 14:17:42 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/05/02 20:25:37 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bmp_parser.h"
 
-void	fill_pixels(unsigned *bmp_pixels, t_bmp_24 *pixels, int width, \
-						int height)
+int		get_read_offset(unsigned short bit)
 {
-	int		x;
-	int		y;
-	int		i;
+	if (bit == (unsigned short)1)
+		return (sizeof(char));
+	return (sizeof(t_bmp_24));
+}
 
-	y = -1;
-	i = 0;
-	while (++y < height)
+void	read_bmp(int fd, t_bmp *bmpfile)
+{
+	void	*pixels;
+	int			offset;
+	int			width;
+	int			height;
+
+	offset = get_read_offset(bmpfile->info.biBitCount);
+	width = bmpfile->info.width;
+	height = bmpfile->info.height;
+	pixels = ft_memalloc(offset * bmpfile->pixel_count);
+	if (!read(fd, pixels, offset * bmpfile->pixel_count))
 	{
-		x =-1;
-		while (++x < width)
-		{
-			bmp_pixels[i++] = bit24_pixel_to_hex(pixels[x + (height - y - 1) \
-													* width]);
-		}
+		ft_putendl_fd("Une erreur est survenue lors de la lecture.", 2);
+		return ;
 	}
+	if (offset == 1)
+		return fill_pixels_1(bmpfile->pixels, (char*)pixels, width, height);
+	return (fill_pixels_24(bmpfile->pixels, (t_bmp_24*)pixels, width, height));
 }
 
 t_bmp	ui_getbmp(char *path)
 {
 	int				fd;
-	t_bmp_24		*pixels;
 	t_bmp			bmpfile;
 
 	ft_bzero(&bmpfile, sizeof(bmpfile));
@@ -47,17 +54,9 @@ t_bmp	ui_getbmp(char *path)
 	read(fd, &bmpfile, sizeof(bmpfile));
 	ui_putbmp(bmpfile.header, bmpfile.info);
 	bmpfile.pixel_count = bmpfile.info.width * bmpfile.info.height;
-	pixels = ft_memalloc(sizeof(t_bmp_24) * bmpfile.pixel_count);
-	bmpfile.pixels = ft_memalloc(sizeof(unsigned) * bmpfile.pixel_count);
-	if (!pixels || !bmpfile.pixels)
+	if (!(bmpfile.pixels = ft_memalloc(sizeof(unsigned) * bmpfile.pixel_count)))
 		return (bmpfile);
-	if (!read(fd, pixels, sizeof(t_bmp_24) * bmpfile.pixel_count))
-	{
-		ft_putendl_fd("Une erreur est survenue lors de la lecture.", 2);
-		return (bmpfile);
-	}
-	fill_pixels(bmpfile.pixels, pixels, bmpfile.info.width, \
-					bmpfile.info.height);
+	read_bmp(fd, &bmpfile);
 	close(fd);
 	return (bmpfile);
 }
