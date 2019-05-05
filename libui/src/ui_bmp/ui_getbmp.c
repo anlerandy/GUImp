271afe11d3c6b056ca2ce37bfad52751107596da
anlerandy <alerandy@student.42.fr>
2019-05-05 16:40:09 +0200

    Updated: Fillers.

--- libui/src/ui_bmp/ui_getbmp.c
+++ libui/src/ui_bmp/ui_getbmp.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 03:54:28 by alerandy          #+#    #+#             */
-/*   Updated: 2019/05/05 14:56:53 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/05/05 16:38:59 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		get_read_offset(unsigned short bit)
 	if (bit == (unsigned short)1)
 		return (sizeof(char));
 	if (bit == (unsigned short)32)
-		return (sizeof(int));
+		return (sizeof(unsigned));
 	return (sizeof(t_bmp_24));
 }
 
@@ -42,14 +42,16 @@ void	read_bmp(int fd, t_bmp *bmpfile)
 	if (bit == 1)
 		return fill_pixels_1(bmpfile->pixels, (char*)pixels, width, height);
 	if (bit == 32)
-		return fill_pixels_32(bmpfile->pixels, (int*)pixels, width, height);
+		return fill_pixels_32(bmpfile->pixels, (unsigned*)pixels, width, height);
 	return (fill_pixels_24(bmpfile->pixels, (t_bmp_24*)pixels, width, height));
 }
 
 t_bmp	ui_getbmp(char *path)
 {
-	int				fd;
-	t_bmp			bmpfile;
+	int			fd;
+	t_bmp		bmpfile;
+	int			skip;
+	unsigned	sizeHeader;
 
 	ft_bzero(&bmpfile, sizeof(bmpfile));
 	if ((fd = open(path, O_RDWR)) == -1)
@@ -57,8 +59,12 @@ t_bmp	ui_getbmp(char *path)
 		ft_putendl_fd("Le fichier n'existe pas.", 2);
 		return (bmpfile);
 	}
-	read(fd, &bmpfile, sizeof(bmpfile));
+	sizeHeader = sizeof(t_bmp_header) + sizeof(t_bmp_file);
+	read(fd, &bmpfile, sizeHeader);
 	ui_putbmp(bmpfile.header, bmpfile.info);
+	skip = bmpfile.header.bfOffBits - sizeHeader;
+	if ((bmpfile.palette = ft_memalloc(sizeof(char) * skip + 1)))
+		read(fd, bmpfile.palette, skip);
 	bmpfile.pixel_count = bmpfile.info.width * bmpfile.info.height;
 	if (!(bmpfile.pixels = ft_memalloc(sizeof(unsigned) * bmpfile.pixel_count)))
 		return (bmpfile);
