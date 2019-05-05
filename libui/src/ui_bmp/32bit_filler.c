b9bee50a776c3140ecb206bb4f4d003c854d475b
anlerandy <alerandy@student.42.fr>
2019-05-05 14:57:39 +0200

    Added: read 32bit img.

--- libui/src/ui_bmp/32bit_filler.c
+++ libui/src/ui_bmp/32bit_filler.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   24bit_filler.c                                     :+:      :+:    :+:   */
+/*   32bit_filler.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/02 20:25:53 by alerandy          #+#    #+#             */
-/*   Updated: 2019/05/05 14:48:42 by alerandy         ###   ########.fr       */
+/*   Created: 2019/05/05 14:46:11 by alerandy          #+#    #+#             */
+/*   Updated: 2019/05/05 14:55:50 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bmp_parser.h"
 
-void	fill_pixels_24(unsigned *bmp_pixels, t_bmp_24 *pixels, int width, \
+void	fill_pixels_32(unsigned *bmp_pixels, int *pixels, int width, \
 						int height)
 {
 	int		x;
@@ -25,7 +25,6 @@ void	fill_pixels_24(unsigned *bmp_pixels, t_bmp_24 *pixels, int width, \
 	{
 		x =-1;
 		while (++x < width)
-			bmp_pixels[i++] = bit24_pixel_to_hex(pixels[x + (height - y - 1) \
-													* width]);
+			bmp_pixels[i++] = pixels[x + (height - y - 1) * width];
 	}
 }
