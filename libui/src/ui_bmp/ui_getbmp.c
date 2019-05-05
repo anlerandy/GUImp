44063a166f5745b7765ba17721b3f2b3f7fbfdfe
anlerandy <alerandy@student.42.fr>
2019-05-05 14:57:39 +0200

    Added: read 32bit img.

--- libui/src/ui_bmp/ui_getbmp.c
+++ libui/src/ui_bmp/ui_getbmp.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 03:54:28 by alerandy          #+#    #+#             */
-/*   Updated: 2019/05/05 14:24:10 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/05/05 14:56:53 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,21 @@ int		get_read_offset(unsigned short bit)
 {
 	if (bit == (unsigned short)1)
 		return (sizeof(char));
+	if (bit == (unsigned short)32)
+		return (sizeof(int));
 	return (sizeof(t_bmp_24));
 }
 
 void	read_bmp(int fd, t_bmp *bmpfile)
 {
-	void	*pixels;
-	int		offset;
-	int		width;
-	int		height;
+	void			*pixels;
+	int				offset;
+	int				width;
+	int				height;
+	unsigned short	bit;
 
-	offset = get_read_offset(bmpfile->info.biBitCount);
+	bit = bmpfile->info.biBitCount;
+	offset = get_read_offset(bit);
 	width = bmpfile->info.width;
 	height = bmpfile->info.height;
 	pixels = ft_memalloc(offset * bmpfile->pixel_count);
@@ -35,8 +39,10 @@ void	read_bmp(int fd, t_bmp *bmpfile)
 		ft_putendl_fd("Une erreur est survenue lors de la lecture.", 2);
 		return ;
 	}
-	if (offset == 1)
+	if (bit == 1)
 		return fill_pixels_1(bmpfile->pixels, (char*)pixels, width, height);
+	if (bit == 32)
+		return fill_pixels_32(bmpfile->pixels, (int*)pixels, width, height);
 	return (fill_pixels_24(bmpfile->pixels, (t_bmp_24*)pixels, width, height));
 }
 
