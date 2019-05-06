5fa0aa4c3fdbfc2ee330c621d5912b7b95daf215
anlerandy <alerandy@student.42.fr>
2019-05-06 16:17:59 +0200

    Fixed: 32Bit.

--- libui/src/ui_bmp/ui_pixtohex.c
+++ libui/src/ui_bmp/ui_pixtohex.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 03:31:05 by alerandy          #+#    #+#             */
-/*   Updated: 2019/05/05 20:58:26 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/05/05 21:46:57 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ unsigned	bit32_pixel_to_hex(t_bmp_32 pixel)
 	unsigned	a;
 
 	a = pixel.a * 256 * 256 * 256;
-	a = pixel.r * 256 * 256;
+	a += pixel.r * 256 * 256;
 	a += pixel.g * 256;
 	a += pixel.b;
 	return (a);
