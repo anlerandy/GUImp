a71b49f3e2a26b26042cd80aa767207415ab6035
Andy LERANDY <alerandy@student.42.fr>
2019-05-10 19:28:59 +0200

    Minor.

--- libui/src/ui_png/ui_write_data.c
+++ libui/src/ui_png/ui_write_data.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 01:34:34 by alerandy          #+#    #+#             */
-/*   Updated: 2019/05/10 15:58:43 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/05/10 19:16:56 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,7 @@ void	write_data(t_png *png, t_png_chunk chunk)
 {
 	void	*tmp;
 
-	if (!png->raw_data)
-	{
-		if (!(png->raw_data = ft_memalloc(chunk.length)))
-		{
-			ft_putendl_fd("Echec d'allocations de memoire.", 2);
-			return ;
-		}
-		ft_memcpy(png->raw_data, chunk.data, chunk.length);
-		png->raw_size = chunk.length;
-		free(chunk.data);
-		chunk.data = NULL;
-		return ;
-	}
+
 	tmp = png->raw_data;
 	if (!(png->raw_data = ft_memalloc(chunk.length + png->raw_size)))
 	{
