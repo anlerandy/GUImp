d2f8a3b14f3c4920f81fc482851120bf49c09c8b
Eliott SUITS <esuits@e3r12p21.42.fr>
2019-06-11 07:12:24 +0200

    normed

--- libui/src/ui_layers/ui_render_layer.c
+++ libui/src/ui_layers/ui_render_layer.c
@@ -14,12 +14,41 @@
 #include "bmp_parser.h"
 #include "libui_layers.h"
 #include "libui_tools.h"
+#include "vectors.h"
 
-void	ui_render_layer(t_ui_win **win, t_ui_layer layer)
+static void		ui_render_layer_rescale(t_ui_win **win, t_ui_layer layer)
+{
+	t_ui_win	*tmp;
+	t_isize		l;
+	unsigned	*dst;
+	unsigned	*src;
+	t_vec2		scale;
+
+	tmp = *win;
+	dst = (unsigned *)tmp->surf->pixels;
+	src = (unsigned *)layer.pixels;
+	scale.x = (double)layer.rescale_w / (double)layer.width;
+	scale.y = (double)layer.rescale_h / (double)layer.height;
+	l.x = (layer.y >= 0) ? layer.y : 0;
+	while (l.x < layer.height * scale.y + layer.y && l.x <= tmp->surf->h)
+	{
+		l.y = (layer.x >= 0) ? layer.x : 0;
+		while (l.y < (layer.width * scale.x) + layer.x && l.y <= tmp->surf->w)
+		{
+			dst[l.y + l.x * tmp->surf->w] = src[((int)((l.y - layer.x)
+				/ scale.x) + (int)((l.x - layer.y) / scale.y) * tmp->surf->w)];
+			l.y++;
+		}
+		l.x++;
+	}
+	SDL_UpdateWindowSurface(tmp->sdl_ptr);
+}
+
+void			ui_render_layer(t_ui_win **win, t_ui_layer layer)
 {
 	t_ui_win	*tmp;
 	unsigned	i;
-	unsigned	limit_w;
+	int			limit_w;
 	unsigned	*src;
 	unsigned	*dst;
 
@@ -27,9 +56,13 @@ void	ui_render_layer(t_ui_win **win, t_ui_layer layer)
 	src = layer.pixels;
 	dst = (unsigned *)tmp->surf->pixels;
 	i = 0;
-	limit_w = tmp->surf->w - layer.x < (int)layer.rescale_w \
-				? tmp->surf->w : layer.rescale_w;
-	while (layer.rescale_h > i || tmp->surf->h > (int)i)
+	limit_w = tmp->surf->w - layer.x < (int)layer.rescale_w
+				? tmp->surf->w : 2 * tmp->surf->w - layer.rescale_w - layer.x;
+	if (limit_w < 0)
+		return ;
+	if (layer.width != layer.rescale_w || layer.height != layer.rescale_h)
+		return (ui_render_layer_rescale(win, layer));
+	while (layer.rescale_h > i && tmp->surf->h > (int)i)
 	{
 		ft_memcpy(dst + layer.x, src, limit_w * sizeof(unsigned));
 		src += layer.width;
