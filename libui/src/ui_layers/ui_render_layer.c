488114ad558f67ffd86cfe2b847a221c128343d5
Eliott SUITS <esuits@e3r12p21.42.fr>
2019-06-11 06:08:43 +0200

    added rescale for layers, still need to do inverted rescale

--- libui/src/ui_layers/ui_render_layer.c
+++ libui/src/ui_layers/ui_render_layer.c
@@ -14,12 +14,41 @@
 #include "bmp_parser.h"
 #include "libui_layers.h"
 #include "libui_tools.h"
+#include "vectors.h"
+
+static void	ui_render_layer_rescale(t_ui_win **win, t_ui_layer layer)
+{
+	t_ui_win *tmp;
+	int i;
+	int j;
+	unsigned *dst;
+	unsigned *src;
+	t_vec2 scale;
+
+	tmp = *win;
+	dst = (unsigned *)tmp->surf->pixels;
+	src = (unsigned *)layer.pixels;
+	scale.x = layer.rescale_w / layer.width;
+	scale.y = layer.rescale_h / layer.height;
+	j = (layer.y >= 0) ? layer.y : 0;	
+	while (j < layer.height * scale.y + layer.y && j <= tmp->surf->h)
+	{
+		i = (layer.x >= 0) ? layer.x : 0;
+		while (i < (layer.width * scale.x) + layer.x && i <= tmp->surf->w)
+		{
+			dst[i + j * tmp->surf->w] = src[((int)((i - layer.x) / scale.x) + (int)((j - layer.y) / scale.y) * tmp->surf->w)];
+			i++;
+		}
+		j++;
+	}
+	SDL_UpdateWindowSurface(tmp->sdl_ptr);
+}
 
 void	ui_render_layer(t_ui_win **win, t_ui_layer layer)
 {
 	t_ui_win	*tmp;
 	unsigned	i;
-	unsigned	limit_w;
+	int			limit_w;
 	unsigned	*src;
 	unsigned	*dst;
 
@@ -28,8 +57,15 @@ void	ui_render_layer(t_ui_win **win, t_ui_layer layer)
 	dst = (unsigned *)tmp->surf->pixels;
 	i = 0;
 	limit_w = tmp->surf->w - layer.x < (int)layer.rescale_w \
-				? tmp->surf->w : layer.rescale_w;
-	while (layer.rescale_h > i || tmp->surf->h > (int)i)
+				? tmp->surf->w : 2 * tmp->surf->w - layer.rescale_w - layer.x;
+	if (limit_w < 0)
+		return ;
+	if (layer.width != layer.rescale_w || layer.height != layer.rescale_h)
+	{
+		ui_render_layer_rescale(win, layer);
+		return ;
+	}
+	while (layer.rescale_h > i && tmp->surf->h > (int)i)
 	{
 		ft_memcpy(dst + layer.x, src, limit_w * sizeof(unsigned));
 		src += layer.width;
@@ -38,3 +74,4 @@ void	ui_render_layer(t_ui_win **win, t_ui_layer layer)
 	}
 	SDL_UpdateWindowSurface(tmp->sdl_ptr);
 }
+
