c0ff5f4c01030c0d4067040142ce65c462b3b131
Guilhem Smith <gsmith@student.42.fr>
2019-06-12 16:23:53 +0200

    Fusion des fichiers de suppresion des fenêtres et des events.

--- libui/src/ui_event/ui_del_event.c
+++ libui/src/ui_event/ui_del_event.c
@@ -6,7 +6,7 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 14:06:27 by gsmith            #+#    #+#             */
-/*   Updated: 2019/05/07 17:50:21 by gsmith           ###   ########.fr       */
+/*   Updated: 2019/06/12 16:22:58 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,9 @@
 void			ui_del_event(t_ui_univers *univers, unsigned int event_id[2])
 {
 	rb_remove(&(univers->events), &event_id, &ui_cmp_event_id, &ui_free_event);
+}
+
+void			ui_clear_events(t_ui_univers *univers)
+{
+	rb_clear_tree(&(univers->events), &ui_free_event);
 }
--- libui/src/ui_window/ui_del_window.c
+++ libui/src/ui_window/ui_del_window.c
@@ -6,7 +6,7 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 13:40:12 by gsmith            #+#    #+#             */
-/*   Updated: 2019/04/27 15:41:07 by gsmith           ###   ########.fr       */
+/*   Updated: 2019/06/12 16:18:19 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,9 @@ void		ui_del_window(t_ui_univers *univers, int win_id)
 		return ;
 	rb_remove(&(univers->windows), (void *)&win_id, &ui_cmp_window_id, \
 		&ui_free_window);
+}
+
+void		ui_clear_all_windows(t_ui_univers *univers)
+{
+	rb_clear_tree(&(univers->windows), &ui_free_window);
 }
