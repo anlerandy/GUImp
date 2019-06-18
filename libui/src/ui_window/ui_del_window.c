84b1bcfdb7fc803260d3742b0ef329d7e9b1eade
Guilhem Smith <gsmith@student.42.fr>
2019-06-18 11:49:43 +0200

    La suppression de fenêtre gère les mères et les blocages.

--- libui/src/ui_window/ui_del_window.c
+++ libui/src/ui_window/ui_del_window.c
@@ -6,34 +6,53 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 13:40:12 by gsmith            #+#    #+#             */
-/*   Updated: 2019/06/13 15:32:49 by gsmith           ###   ########.fr       */
+/*   Updated: 2019/06/18 11:49:14 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "libui_tools.h"
 
+static void	clear_child(t_ui_univers *univers, int win_id)
+{
+	t_ui_win		**childs;
+	unsigned int	child_id;
+	unsigned int	i;
+
+	if (!(childs = (t_ui_win **)rb_search_all(univers->windows, \
+			(void *)&win_id, &ui_cmp_mother)))
+		return ;
+	i = -1;
+	while (childs[++i])
+	{
+		child_id = childs[i]->id;
+		rb_remove(&(univers->windows), (void *)&child_id, \
+			&ui_cmp_window_id, &ui_free_window);
+	}
+}
+
+static void	clear_block(t_ui_univers *univers, int win_id)
+{
+	t_ui_win		**blocked;
+	unsigned int	i;
+
+	if (!(blocked = (t_ui_win **)rb_search_all(univers->windows, \
+			(void *)&win_id, &ui_cmp_blocker)))
+		return ;
+	i = -1;
+	while (blocked[++i])
+		blocked[i]->blocked = 0;
+}
+
 void		ui_del_window(t_ui_univers *univers, int win_id)
 {
 	t_ui_win		*win;
-	void			**childs;
-	unsigned int	child_id;
-	unsigned int	i;
 
 	if (!(win = rb_search_infix(univers->windows, (void *)&win_id, \
 			&ui_cmp_window_id)) || win->blocked != 0)
 		return ;
-	childs = rb_search_all(univers->windows, (void *)&win_id, &ui_cmp_mother);
-	if (childs)
-	{
-		i = -1;
-		while (childs[++i])
-		{
-			child_id = ((t_ui_win *)childs[i])->id;
-			rb_remove(&(univers->windows), (void *)&child_id, \
-				&ui_cmp_window_id, &ui_free_window);
-		}
-	}
+	clear_child(univers, win_id);
+	clear_block(univers, win_id);
 	rb_remove(&(univers->windows), (void *)&win_id, &ui_cmp_window_id, \
 		&ui_free_window);
 }
