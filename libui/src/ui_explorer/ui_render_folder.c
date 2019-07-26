3f5e8cbfb55514b5959574a7f90563fec8c54c98
Andy <alerandy@student.42.fr>
2019-07-26 17:48:31 +0200

    Updated: Place selected dir.

--- libui/src/ui_explorer/ui_render_folder.c
+++ libui/src/ui_explorer/ui_render_folder.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/25 16:45:05 by alerandy          #+#    #+#             */
-/*   Updated: 2019/07/26 13:26:50 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/07/26 17:08:26 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,10 @@ void	ui_render_folder(t_ui_folder *folder)
 	if (!folder || !folder->win || !folder->layers || !folder->ls)
 		return (ft_putendl_fd("Echec de rendu Folder.", 2));
 	pixels = folder->win->surf->w * folder->win->surf->h;
-	// ui_memuset((void*)(folder->win->surf), 0xff01003b, pixels);
-	// ui_memuset(folder->win->surf, 0xff001aff, folder->win->surf->w * 50 \
-	// 											* sizeof(unsigned));
 	while (++i < folder->ls->files_amount + 1)
 	{
 		layer = folder->layers[i];
-		layer->x = !i ? 35 : 15;
+		layer->x = (!i ? 35 : 15) + (i && i == (int)folder->selected) * 20;
 		layer->y = !i ? 10 : i * 35 + 10;
 		ui_render_layer(&folder->win, layer);
 	}
