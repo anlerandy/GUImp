b4cedbb0447df12655c9cb144bf3ad4228f144b8
Andy <alerandy@student.42.fr>
2019-07-30 23:56:26 +0200

    Added: Colors to the files of a folder.

--- libui/src/ui_explorer/ui_open_folder.c
+++ libui/src/ui_explorer/ui_open_folder.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/25 12:50:04 by alerandy          #+#    #+#             */
-/*   Updated: 2019/07/26 16:56:13 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/07/30 23:55:24 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,36 @@
 #include "libui.h"
 #include "libui_ttf.h"
 #include "libui_explorer.h"
+#include <fcntl.h>
 
-t_ui_folder	*ui_get_folder(char *path, t_ui_win *win)
+static inline t_ui_layer	*get_text_layer(char *file, char *path)
+{
+	unsigned	color;
+	int			fd;
+	DIR			*dir;
+	char		*tmp;
+
+	if (!file || !path)
+		return NULL;
+	tmp = ft_strjoin(path, file);
+	if ((dir = opendir(tmp)))
+	{
+		color = 0xff00ff00;
+		closedir(dir);
+	}
+	else if ((fd = open(tmp, O_RDONLY)) > 0)
+	{
+		color = 0xff00eaff;
+		close(fd);
+	}
+	else
+		color = 0xffff0000;
+	ft_strdel(&tmp);
+	return (ui_ttf_to_layer(NULL, file, \
+			(t_ui_ttf_param){0, 0, 0, 30, 0, 0, color}));
+}
+
+t_ui_folder					*ui_get_folder(char *path, t_ui_win *win)
 {
 	int				i;
 	t_ui_folder		*folder;
@@ -38,14 +66,15 @@ t_ui_folder	*ui_get_folder(char *path, t_ui_win *win)
 	folder->layers[0] = ui_ttf_to_layer(NULL, folder->ls->path, \
 			(t_ui_ttf_param){0, 0, 0, 30, 0, 0, 0xffa1a1a1});
 	while (folder->ls->files[++i])
-		folder->layers[i + 1] = ui_ttf_to_layer(NULL, folder->ls->files[i], \
-			(t_ui_ttf_param){0, 0, 0, 30, 0, 0, 0xffffffff});
+		folder->layers[i + 1] = get_text_layer(folder->ls->files[i], \
+														folder->ls->path);
 	if (win)
 		folder->win = NULL;
 	return (folder);
 }
 
-t_ui_folder	*ui_open_folder(t_ui_univers *univers, char *path, t_ui_win *win)
+t_ui_folder					*ui_open_folder(t_ui_univers *univers, char *path, \
+																t_ui_win *win)
 {
 	t_ui_folder		*folder;
 
