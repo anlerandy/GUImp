3dd5eceb584d933905dc9ef217f229f779abc828
Andy LERANDY <alerandy@student.42.fr>
2019-08-10 15:49:07 +0200

    Fixed: ft_strcmp.

--- libft/srcs/string/ft_strcmp.c
+++ libft/srcs/string/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 14:55:54 by alerandy          #+#    #+#             */
-/*   Updated: 2019/04/21 17:00:01 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/08/10 15:30:21 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		ft_strcmp(const char *s1, const char *s2)
 	str2 = (unsigned char *)s2;
 	n = ft_strlen(s1);
 	i = ft_strlen(s2);
-	n = n < i ? i : n;
+	n = n > i ? i : n;
 	i = 0;
 	if (n == 0)
 		return (0);
