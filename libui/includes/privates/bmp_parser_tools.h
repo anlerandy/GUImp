504226116b09c927cc66c49ff13c4b41bfe89426
anlerandy <alerandy@student.42.fr>
2019-10-06 13:26:13 +0200

    Minor: Indented.

--- libui/includes/privates/bmp_parser_tools.h
+++ libui/includes/privates/bmp_parser_tools.h
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 16:48:19 by alerandy          #+#    #+#             */
-/*   Updated: 2019/10/06 12:52:23 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/10/06 13:25:33 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 #include "bmp_parser.h"
 #include "ui_shared.h"
 
-t_bmp *print_parse_error(int error, t_bmp *bmp, char *path);
-int validate_bmp(t_bmp *bmp);
+t_bmp	*print_parse_error(int error, t_bmp *bmp, char *path);
+int		validate_bmp(t_bmp *bmp);
 
-void fill_pixels_32(unsigned *bmp_pixels, t_bgra *pixels,
+void	fill_pixels_32(unsigned *bmp_pixels, t_bgra *pixels,
 										int width, int height);
-void fill_pixels_24(unsigned *bmp_pixels, t_bgr *pixels,
+void	fill_pixels_24(unsigned *bmp_pixels, t_bgr *pixels,
 										int width, int height);
-void fill_pixels_16(unsigned *bmp_pixels, unsigned short *pixels,
+void	fill_pixels_16(unsigned *bmp_pixels, unsigned short *pixels,
 										int width, int height);
-void fill_pixels_index(t_bmp *bmp, char *pixels, int width,
+void	fill_pixels_index(t_bmp *bmp, char *pixels, int width,
 											 int height);
-void fill_pixels_1(unsigned *bmp_pixels, char *pixels,
+void	fill_pixels_1(unsigned *bmp_pixels, char *pixels,
 									 int width, int height);
 
 #endif
