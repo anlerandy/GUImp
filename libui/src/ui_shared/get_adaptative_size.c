0064ce449ebf49947aef83c568820956987a6c2b
anlerandy <alerandy@student.42.fr>
2019-11-04 08:19:20 -0300

    Updated: Refactor.

--- libui/src/ui_shared/get_adaptative_size.c
+++ libui/src/ui_shared/get_adaptative_size.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 06:45:36 by anlerandy         #+#    #+#             */
-/*   Updated: 2019/11/04 07:49:17 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/11/04 08:18:08 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,16 @@ t_pos       get_adaptative_size(unsigned width, unsigned height)
     SDL_DisplayMode     dm;
 
     SDL_GetCurrentDisplayMode(0, &dm);
-    if (width >= (unsigned)dm.w)
+    if (width >= (unsigned)dm.w || width <= 500)
     {
-        size.x = dm.w - 200;
+        size.x = width <= 500 ? 600 : dm.w - 200;
         size.y = size.x * 100. / width / 100. * height;
         return get_adaptative_size((unsigned)size.x, (unsigned)size.y);
     }
-    if (height >= (unsigned)dm.h)
+    if (height >= (unsigned)dm.h || height <= 500)
     {
-        size.y = dm.h - 200;
+        size.y = height <= 500 ? 600 : dm.h - 200;
         size.x = size.y * 100. / height / 100. * width;
-        return get_adaptative_size((unsigned)size.x, (unsigned)size.y);
-    }
-    if (height <= 500)
-    {
-        size.y = 600;
-        size.x = size.y * 100. / height / 100. * width;
-        return get_adaptative_size((unsigned)size.x, (unsigned)size.y);
-    }
-    if (width <= 500)
-    {
-        size.x = 600;
-        size.y = size.x * 100. / width / 100. * height;
         return get_adaptative_size((unsigned)size.x, (unsigned)size.y);
     }
     return ((t_pos){(unsigned)width, (unsigned)height});
