9a99e1d6cb009e5abc6f1ad074fe95eac64b4941
anlerandy <alerandy@student.42.fr>
2019-11-05 15:03:34 -0300

    Updated: More security on adaptative.

--- libui/src/ui_shared/get_adaptative_size.c
+++ libui/src/ui_shared/get_adaptative_size.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 06:45:36 by anlerandy         #+#    #+#             */
-/*   Updated: 2019/11/04 19:33:44 by alerandy         ###   ########.fr       */
+/*   Updated: 2019/11/05 15:02:12 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,25 @@
 
 t_pos       get_adaptative_size(unsigned width, unsigned height)
 {
-    t_pos               size;
-    SDL_DisplayMode     dm;
+    t_pos                   size;
+    SDL_DisplayMode         dm;
+    static unsigned char    iter = 0;
 
-    SDL_GetCurrentDisplayMode(0, &dm);
-    if (width >= (unsigned)dm.w || width <= 500)
+    if (SDL_GetCurrentDisplayMode(0, &dm))
+        return ((t_pos){(int)width, (int)height});
+    ++iter;
+    if ((width >= (unsigned)dm.w || width <= 500) && iter < 6)
     {
         size.x = width <= 500 ? 600 : dm.w - 200;
         size.y = size.x * 100. / width / 100. * height;
         return get_adaptative_size((unsigned)size.x, (unsigned)size.y);
     }
-    if (height > (unsigned)dm.h - 350 || height <= 500)
+    if ((height > (unsigned)dm.h - 350 || height <= 500) && iter < 6)
     {
         size.y = height <= 500 ? 600 : dm.h - 350;
         size.x = size.y * 100. / height / 100. * width;
         return get_adaptative_size((unsigned)size.x, (unsigned)size.y);
     }
-    return ((t_pos){(unsigned)width, (unsigned)height});
+    iter = 0;
+    return ((t_pos){(int)width, (int)height});
 }
