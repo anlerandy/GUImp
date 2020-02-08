72b117b82a62dbfca0980b23f453b29f6ad963d8
Andy LERANDY <alerandy@student.42.fr>
2020-02-08 18:39:13 +0100

    Fixed: Mouse move crash on `null` window.

--- libui/src/ui_event/default/event_mouse_motion.c
+++ libui/src/ui_event/default/event_mouse_motion.c
@@ -6,7 +6,7 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 18:16:29 by alerandy          #+#    #+#             */
-/*   Updated: 2020/02/08 17:18:10 by alerandy         ###   ########.fr       */
+/*   Updated: 2020/02/08 18:33:20 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void	callback_mouse_motion(t_ui_univers **uni, void *data, \
 	t_ui_win		*win;
 
 	(void)data;
-	win = ui_get_window_by_id(*uni, event.win_id);
+	if (!(win = ui_get_window_by_id(*uni, event.win_id)))
+		return ;
 	if (win->current_elem && win->current_elem->state == UI_ELEM_STATE_HOVER)
 		win->current_elem->state = UI_ELEM_STATE_ACTIVE;
 	if (!(elem = ui_get_elem_by_position(win, event.x, event.y)))
